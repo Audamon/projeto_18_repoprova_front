68c0823a749687e717e04bd6f266991718c4d552
Matheus Galdino <matheus@respondeai.com.br>
2022-01-04 12:24:32 -0300

    preparando o review

--- src/TopBar/TopBar.js
+++
@@ -1,9 +0,0 @@
-import { TopBarHeader, Title } from './TopBar_style.js';
-
-export default function TopBar() {
-  return (
-      <TopBarHeader>
-          <Title>RepoProva</Title>
-      </TopBarHeader>
-  );
-}
